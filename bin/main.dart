5cdf120f893c41bafb6eaf909efb14beacf549f1
AymanTheGoat <minicuneyt@gmail.com>
2024-09-09 14:41:44 +0100

    Initial commit :DDD

---
+++ bin/main.dart
@@ -0,0 +1,231 @@
+import 'dart:async';
+import 'package:args/command_runner.dart';
+import 'package:dbus/dbus.dart';
+import 'package:polytunes/methods.dart';
+
+// Main function that sets up the CommandRunner
+Future<void> main(List<String> args) async {
+  final DBusClient client = DBusClient.session();
+  final DBusRemoteObject player = DBusRemoteObject(
+    client,
+    name: 'org.mpris.MediaPlayer2.spotify',
+    path: DBusObjectPath('/org/mpris/MediaPlayer2'),
+  );
+
+  final runner = CommandRunner('spotify', 'Control media playback.')
+    ..addCommand(MediaControlCommands(player));
+
+  try {
+    await runner.run(args);
+  } catch (e) {
+    print('Error: ${e.toString()}');
+  } finally {
+    client.close();
+  }
+}
+
+
+// Command for media control (play/pause, next, previous, status, artist, song)
+class MediaControlCommands extends Command {
+  final DBusRemoteObject player;
+
+  MediaControlCommands(this.player) {
+      addSubcommand(PlayPauseCmd(player));
+      addSubcommand(GoNextCmd(player));
+      addSubcommand(GoPreviousCmd(player));
+      addSubcommand(GetTitleCmd(player));
+      addSubcommand(GetArtistCmd(player));
+      addSubcommand(GetStatusCmd(player));
+      addSubcommand(ScrollCmd(player));
+  }
+
+  @override
+  String get name => 'spotify';
+
+  @override
+  String get description => 'Control the media playback (play/pause, next, previous) or get metadata (status, song name, or artist name).';
+  
+  @override
+  void run() async {
+    if (argResults?['previous'])
+      {await goPrevious(player);}
+    else if (argResults?['status']) 
+      {print(await getPlaybackStatus(player));}
+  }
+}
+
+class PlayPauseCmd extends Command{
+  final DBusRemoteObject player;
+  
+  PlayPauseCmd(this.player);
+  
+  @override
+  String get name => 'playpause';
+
+  @override
+  String get description => 'plays / pauses spotify playback';
+
+  @override
+  void run() async{
+    try {
+      await playPause(player);
+    } catch (e) {
+      // 
+    }
+  }
+}
+
+class GoNextCmd extends Command{
+  final DBusRemoteObject player;
+  
+  GoNextCmd(this.player);
+  
+  @override
+  String get name => 'next';
+
+  @override
+  String get description => 'go to next song';
+
+  @override
+  void run() async {
+    try {
+      await goNext(player);
+    } catch (e) {
+     // 
+    }
+  }
+}
+
+class GoPreviousCmd extends Command{
+  final DBusRemoteObject player;
+
+  GoPreviousCmd(this.player);
+
+  @override
+  String get name => 'previous';
+
+  @override
+  String get description => 'go to previous song';
+
+  @override
+  void run() async{
+    try {
+      await goPrevious(player);
+    } catch (e) {
+     // 
+    }
+  }
+}
+
+class GetStatusCmd extends Command{
+  final DBusRemoteObject player;
+
+  GetStatusCmd(this.player);
+
+  @override
+  String get name => 'status';
+
+  @override
+  String get description => 'get current status of playback';
+
+  @override
+  void run() async{
+    try {
+      print(await getPlaybackStatus(player));
+    } catch (e) {
+      //
+    }
+  }
+}
+
+class GetArtistCmd extends Command{
+  final DBusRemoteObject player;
+
+  GetArtistCmd(this.player);
+
+  @override
+  String get name => 'artist';
+
+  @override
+  String get description => 'get name of artist of currently played song';
+
+  @override
+  void run() async{
+    try {
+      print(await getArtistName(player));
+    } catch (e) {
+      //
+    }
+  }
+}
+
+class GetTitleCmd extends Command{
+  final DBusRemoteObject player;
+
+  GetTitleCmd(this.player);
+
+  @override
+  String get name => 'title';
+
+  @override
+  String get description => 'get name of currently played song';
+
+  @override
+  void run() async{
+    try {
+      print(await getSongName(player));
+    } catch (e) {
+      //
+    }
+  }
+}
+
+class ScrollCmd extends Command {
+  final DBusRemoteObject player;
+
+  ScrollCmd(this.player) {
+    argParser
+      ..addOption(
+        'display-length',
+        abbr: 'd',
+        defaultsTo: '30',
+        help: 'The length displayed before text gets cut.',
+      )
+      ..addOption(
+        'interval',
+        abbr: 'i',
+        defaultsTo: '1',
+        help: 'The interval in seconds for updating (scrolling).',
+      )
+      ..addOption(
+        'spacing',
+        abbr: 's',
+        defaultsTo: '10',
+        help: 'The space left at the end of every scroll.',
+      );
+  }
+ 
+  @override
+  String get name => 'scroll';
+
+  @override
+  String get description => 'perpetually scroll song metadata';
+
+  @override
+  void run() async{
+    final int displayLength = int.parse(argResults!['display-length']);
+    final int intervalSeconds = int.parse(argResults!['interval']);
+    final int spacing = int.parse(argResults!['spacing']);
+
+    try {
+      scrollMetaData(
+        player,
+        displayLength: displayLength,
+        intervalSeconds: intervalSeconds,
+        spacing: spacing
+      );
+    } catch (e) {
+      //
+    }
+  }
+}
---
+++ lib/methods.dart
@@ -0,0 +1,123 @@
+// ------------------- Helper methods ---------------------- //
+// Function to control Play/Pause
+import 'dart:async';
+
+import 'package:dbus/dbus.dart';
+
+// Function to control Next
+
+Future<void> playPause(DBusRemoteObject player) async {
+  await player.callMethod(
+    'org.mpris.MediaPlayer2.Player',
+    'PlayPause',
+    [],
+  );
+}
+
+// Function to control Next
+Future<void> goNext(DBusRemoteObject player) async {
+  await player.callMethod('org.mpris.MediaPlayer2.Player', 'Next', []);
+}
+
+// Function to control Previous
+Future<void> goPrevious(DBusRemoteObject player) async {
+  await player.callMethod('org.mpris.MediaPlayer2.Player', 'Previous', []);
+}
+
+// Function to get playback status
+Future<String> getPlaybackStatus(DBusRemoteObject player) async {
+  final DBusValue playbackStatusValue = await player.getProperty(
+    'org.mpris.MediaPlayer2.Player',
+    'PlaybackStatus',
+  );
+  return playbackStatusValue.toNative();
+}   
+
+// Function to get song title
+Future<String> getSongName(DBusRemoteObject player) async {
+  final DBusValue metadata = await player.getProperty(
+    'org.mpris.MediaPlayer2.Player',
+    'Metadata',
+  );
+  final nativeMetadata = metadata.toNative();
+
+  final artist = nativeMetadata["xesam:title"];
+  return artist.toString();
+}
+
+// Function to get song title
+Future<String> getArtistName(DBusRemoteObject player) async {
+  final DBusValue metadata = await player.getProperty(
+    'org.mpris.MediaPlayer2.Player',
+    'Metadata',
+  );
+  final nativeMetadata = metadata.toNative();
+  final artist = nativeMetadata["xesam:artist"].toString();
+  String processedArtistName;
+  if (artist.length > 2) {
+    processedArtistName = artist.substring(1, artist.length - 1);
+  } else {
+    // In case the artist name is too short to be processed
+    return '';
+  }
+  return processedArtistName;
+}
+
+// Function to continuously scroll the song metadata
+void scrollMetaData(DBusRemoteObject player, {int displayLength = 10, int intervalSeconds = 1, int spacing = 10}) {
+  int startIndex = 0; // Start scrolling from the first character
+  String lastText = ''; // Keep track of the last displayed text to detect changes
+
+  Timer.periodic(Duration(seconds: intervalSeconds), (Timer timer) async {
+    // Fetch the current playback status
+    final String playbackStatus = await getPlaybackStatus(player);
+
+    if (playbackStatus == 'Playing') {
+      // Fetch the current metadata
+      final String artist = await getArtistName(player);
+      final String songName = await getSongName(player);
+      
+      // This is for the case where an advertisement is playing
+      // I know this is a weird way to implement it 
+      // But honestly I CBA to clean it up since it works and that's all that matters 
+      String unspacedText;
+      if (artist == '') {
+        unspacedText = songName;
+      }
+      else {
+        unspacedText = "$artist - $songName";
+      }
+      
+      // Combine artist and song name into a single text string
+      final String text = "$unspacedText${' ' * spacing}";
+      final int textLength = text.length;
+
+      // Reset startIndex if text has changed
+      if (text != lastText) {
+        startIndex = 0;
+        lastText = text; // Update lastText to the current text
+      }
+
+      // Check if text length is less than or equal to displayLength
+      if (unspacedText.length <= displayLength) {
+        // Print the full text without scrolling
+        print(text);
+      } else {
+        // Calculate the end index of the substring
+        int endIndex = startIndex + displayLength;
+
+        // Handle the wrapping around of the text for scrolling effect
+        if (endIndex > textLength) {
+          // Wrap around and concatenate the overflowing part from the start of the text
+          print(text.substring(startIndex) + text.substring(0, endIndex % textLength));
+        } else {
+          // Print the current slice of the text
+          print(text.substring(startIndex, endIndex));
+        }
+
+        // Increment the start index for the next scroll
+        startIndex = (startIndex + 1) % textLength;
+      }
+    }
+  });
+}
